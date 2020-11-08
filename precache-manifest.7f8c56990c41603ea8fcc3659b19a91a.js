c9ebdf50b11dd23b9be873862014d204571493db
chess220807@gmail.com <chess220807@gmail.com>
2020-11-08 11:20:14 +0200

    Updates

---
+++ precache-manifest.7f8c56990c41603ea8fcc3659b19a91a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e02d95d361fad6cb6c14790828bc21",
+    "url": "/react_todoforwork/index.html"
+  },
+  {
+    "revision": "44d5d6f1eb62f4b06deb",
+    "url": "/react_todoforwork/static/css/main.04adfe2d.chunk.css"
+  },
+  {
+    "revision": "d5c685f31201228d3676",
+    "url": "/react_todoforwork/static/js/2.e74381a0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react_todoforwork/static/js/2.e74381a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d5d6f1eb62f4b06deb",
+    "url": "/react_todoforwork/static/js/main.f5e40dec.chunk.js"
+  },
+  {
+    "revision": "ef6b373504c0dcfde5f2",
+    "url": "/react_todoforwork/static/js/runtime-main.6f0e08f7.js"
+  }
+]);
